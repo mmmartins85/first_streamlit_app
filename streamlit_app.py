cb733514fc5bb1ea173c699c6ee15ad01fcbc3c9
mmmartins85 <126419039+mmmartins85@users.noreply.github.com>
2023-02-26 15:32:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Eggs Benedict')
+streamlit.text('Hash and Eggs')
+streamlit.text('Denver Omelete')
+streamlit.text('Egg White Omelete')
+streamlit.text('Belgian Waffles')
+streamlit.text('Pancakes')
